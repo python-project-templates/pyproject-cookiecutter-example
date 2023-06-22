0597ce1bbb9519819d6fd84a8766f26a5951c375
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2023-06-22 15:48:59 -0400

    .

--- docs/make.bat
+++ docs/make.bat
@@ -9,7 +9,7 @@ if "%SPHINXBUILD%" == "" (
 )
 set SOURCEDIR=.
 set BUILDDIR=_build
-set SPHINXPROJ=https://github.com/timkpaine/pyproject-cookiecutter
+set SPHINXPROJ=https://github.com/python-project-templates/pyproject-cookiecutter
 
 if "%1" == "" goto help
 
